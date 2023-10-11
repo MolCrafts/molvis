b90b8d50f31d111d46cb597b59c2dc4f301e23c4
Roy Kid <lijichen365@gmail.com>
2023-10-11 17:48:43 +0200

    fix create canvas typo

--- page/src/index.js
+++ page/src/index.js
@@ -4,7 +4,7 @@ document.body.style.width = "100%";
 document.body.style.height = "100%";
 document.body.style.overflow = "hidden";
 
-const canvas = document.createElement("renderCanvas");
+const canvas = document.createElement("canvas");
 canvas.id = "renderCanvas";
 canvas.style.width = "100%";
 canvas.style.height = "100%";
@@ -16,7 +16,7 @@ if (DEV_BUILD) {
 } else {
     assetsHostUrl = "https://nonlocal-assets-host-url/";
 }
-const app = initMolvisApp({canvas, assetsHostUrl});
+const app = initMolvisApp({ canvas: canvas, assetsHostUrl: assetsHostUrl});
 const system = app.system;
 system.add_atom({"xyz": [0, 0, 1]});
 app.run();

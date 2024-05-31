76978f367474a1fa32cd5f5e8828be4592bf44b2
Roy Kid <lijichen365@gmail.com>
2024-05-31 23:56:40 +0200

    [axes] fix axes bug

--- core/src/view/axis.ts
+++ core/src/view/axis.ts
@@ -1,4 +1,4 @@
-import { Scene, MeshBuilder, StandardMaterial, DynamicTexture, Vector3, ArcRotateCamera, Viewport, Color3 } from "@babylonjs/core";
+import { Scene, MeshBuilder, StandardMaterial, DynamicTexture, Vector3, ArcRotateCamera, Viewport, Color3, HemisphericLight } from "@babylonjs/core";
 import { AxesViewer as _AxesView } from "@babylonjs/core/Debug/axesViewer";
 import { World, IRenderable } from "./world";
 
@@ -15,22 +15,20 @@ class Axes {
         this.scene = scene;
         this.axes = new _AxesView(scene, size);
         let font_size = size * 0.2;
-        let xChar = this._make_axis_label("X", "red", font_size);
-        let yChar = this._make_axis_label("Y", "green", font_size);
-        let zChar = this._make_axis_label("Z", "blue", font_size);
-
-        // Position
-        xChar.position = this.axes.xAxis.position.clone();
-        yChar.position = this.axes.yAxis.position.clone();
-        zChar.position = this.axes.zAxis.position.clone();
-        xChar.position.z += 0.4;
-        yChar.position.z += 0.4;
-        zChar.position.z += 0.4;
+        let x_end = this.axes.xAxis.position.clone();
+        let y_end = this.axes.yAxis.position.clone();
+        let z_end = this.axes.zAxis.position.clone();
+        x_end.x += 0.4;
+        y_end.y += 0.4;
+        z_end.z += 0.4;
+        let xChar = this._make_axis_label("X", "red", font_size, x_end);
+        let yChar = this._make_axis_label("Y", "green", font_size, y_end);
+        let zChar = this._make_axis_label("Z", "blue", font_size, z_end);    
 
         // Rotation
-        // xChar.rotation.y = Math.PI/2;
-        yChar.rotation.x = Math.PI / 2;
-        // zChar.rotation.z = Math.PI/2;
+        xChar.rotation.x = Math.PI;
+        yChar.rotation.x = Math.PI;
+        zChar.rotation.z = Math.PI;
 
         // Parent
         xChar.parent = this.axes.xAxis;
@@ -38,16 +36,19 @@ class Axes {
         zChar.parent = this.axes.zAxis;;
     }
 
-    private _make_axis_label(text: string, color: string, size: number) {
-        var dynamicTexture = new DynamicTexture("DynamicTexture", 50, this.scene, true);
+    private _make_axis_label(text: string, color: string, size: number, position: Vector3) {
+        let dynamicTexture = new DynamicTexture("DynamicTexture", 50, this.scene);
         dynamicTexture.hasAlpha = true;
-        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
-        var plane = MeshBuilder.CreatePlane("TextPlane", { size: size }, this.scene);
+        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", false);
+        let plane = MeshBuilder.CreatePlane("TextPlane", { size: size }, this.scene);
         let material = new StandardMaterial("TextPlaneMaterial", this.scene);
         material.backFaceCulling = false;
         material.specularColor = new Color3(0, 0, 0);
         material.diffuseTexture = dynamicTexture;
         plane.material = material;
+        // plan parallel to camera
+        plane.billboardMode = 7;
+        plane.position = position;
         return plane;
     };
 
@@ -68,16 +69,18 @@ class AxesViewer implements IRenderable {
         cameraGizmo.viewport = new Viewport(0.0, 0.0, 0.2, 0.2);
 
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        // var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-        // // Default intensity is 1. Let's dim the light a small amount
-        // light.intensity = 0.7;
+        var light = new HemisphericLight("light", new Vector3(0, 1, 0), this.second_scene);
+        // Default intensity is 1. Let's dim the light a small amount
+        light.intensity = 1;
 
-        // var lightGizmo = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+        light.diffuse = new Color3(1, 1, 1); // 漫射光颜色
+        light.specular = new Color3(0.5, 0.5, 0.5); // 镜面光颜色
+        light.groundColor = new Color3(0.8, 0.8, 0.8); // 地面光颜色
 
         this.axes = new Axes(this.second_scene, 0.8);
 
         // Clone main camera alpha and beta to axis camera
-        world.scene.registerBeforeRender(function () {
+        world.scene.registerBeforeRender(() => {
             cameraGizmo.alpha = world.camera.alpha;
             cameraGizmo.beta = world.camera.beta;
         });
